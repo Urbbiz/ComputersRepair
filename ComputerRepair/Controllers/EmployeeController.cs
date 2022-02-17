a1aeb076453b45970ef82cd98f2dc948b5565510
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2022-02-17 11:58:44 +0200

    migration

---
+++ ComputerRepair/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using ComputerRepair.Data;
+using ComputerRepair.Enteties;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerRepair.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EmployeeController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public void Post(Employee employee)
+        {
+            _context.Add(employee);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ ComputerRepair/Migrations/20220217094820_Init.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerRepair.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Computers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IssueDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Computers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Maitences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ComputerId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maitences", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Maitences_Computers_ComputerId",
+                        column: x => x.ComputerId,
+                        principalTable: "Computers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComputerEmployee",
+                columns: table => new
+                {
+                    ComputersId = table.Column<int>(type: "int", nullable: false),
+                    EmployeesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComputerEmployee", x => new { x.ComputersId, x.EmployeesId });
+                    table.ForeignKey(
+                        name: "FK_ComputerEmployee_Computers_ComputersId",
+                        column: x => x.ComputersId,
+                        principalTable: "Computers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ComputerEmployee_Employees_EmployeesId",
+                        column: x => x.EmployeesId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComputerEmployee_EmployeesId",
+                table: "ComputerEmployee",
+                column: "EmployeesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maitences_ComputerId",
+                table: "Maitences",
+                column: "ComputerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ComputerEmployee");
+
+            migrationBuilder.DropTable(
+                name: "Maitences");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Computers");
+        }
+    }
+}
